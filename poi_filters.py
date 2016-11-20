176a58227452e8ed0ed11a3d704b6b0362f03b8a
Mark Fickett <mark.fickett@gmail.com>
2016-11-19 20:37:58 -0500

    Update sign filter for 1.11 entity storage format.

--- poi_filters.py
+++ poi_filters.py
@@ -1,17 +1,20 @@
 # https://overviewer.readthedocs.org/en/latest/signs/
 # POI filters to render player locations and spawn points, and sign text.
+import json
 import time
 
 _AVATAR_URL_T = 'http://overviewer.org/avatar/%s'
 _TIME_FORMAT = '%Y %b %d %H:%M %Z'
 
-
 def _SignText(poi):
-  if poi['id'] != 'Sign':
+  poi_id = poi['id']
+  if poi_id not in ('Sign', 'minecraft:sign'):
     return
   texts = []
   for n in range(1, 5):
     t = poi.get('Text%d' % n)
+    if poi_id == 'minecraft:sign':
+      t = json.loads(t)['text']
     texts.append('' if t == 'null' else t)
   if any(texts):  # Omit blank signs (used for structure only).
     texts.append('y = %s' % poi['y'])
@@ -24,7 +27,7 @@ def _PlayerLocation(poi):
   # depends on __getattr__ to trigger UUID-to-name lookup
   name = poi['EntityId']
   if not name:
-      return  # probably a leftover from before the UUID change <= 2014
+    return  # probably a leftover from before the UUID change <= 2014
   poi['icon'] = _AVATAR_URL_T % name
   return 'location for %s as of %s' % (
       name, time.strftime(_TIME_FORMAT, poi['time']))
